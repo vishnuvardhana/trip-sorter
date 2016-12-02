c6a615c90a99e0c42230d762323c9f7ba4b70ba9
vishnuvardhana <vishnu@i3software.in>
2016-12-02 17:56:16 +0530

    making changes for hosting in heroku

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ const  path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/dist'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(port);
 console.log("Server Started "+port);
