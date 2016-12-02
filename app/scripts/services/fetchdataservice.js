186aa699fc9107dd402ca91ac6e58fe2662ef64c
vishnuvardhana <vishnu@i3software.in>
2016-12-02 17:33:11 +0530

    1.heroku hosting using node 2.removing unwanted templates and files

--- app/scripts/services/fetchdataservice.js
+++ app/scripts/services/fetchdataservice.js
@@ -9,17 +9,13 @@
  */
 angular.module('travelFinderApp')
     .factory('fetchDataService', function($http, API_URL) {
-
+        /**
+         * [getRemoteData get data from remote using API_URL]
+         * @return {promise} return https promise for the request
+         */
         var getRemoteData = function() {
-           
                return $http({ "url": API_URL, method: "get" });
-
-          
-            
-
         };
-
-
         return {
             getRemoteData: getRemoteData
         };
